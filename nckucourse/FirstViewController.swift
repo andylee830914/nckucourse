87a0f9559e13fbe1ebf11ae3abfd74065d4c3461
Yu Hsun Lee <andylee0914.tw@gmail.com>
2015-07-10 00:07:29 +0800

    Initial Commit

---
+++ nckucourse/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  nckucourse
+//
+//  Created by Lee Yu Hsun on 2015/7/10.
+//  Copyright (c) 2015年 Lee Yu Hsun. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
